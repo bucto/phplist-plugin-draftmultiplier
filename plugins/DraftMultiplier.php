eb835356b90674a7dbb760c3e0edf6c88897bdd3
TomBu <t.buecken@gmail.com>
2025-12-23 08:33:42 +0100

    1.1.11

--- plugins/DraftMultiplier.php
+++ plugins/DraftMultiplier.php
@@ -3,7 +3,7 @@
 class DraftMultiplier extends phplistPlugin
 {
     public $name = 'DraftMultiplier';
-    public $version = '1.1.10';
+    public $version = '1.1.11';
     public $authors = 'bucto';
     public $enabled = true;
     public $description = 'Pro tool to duplicate campaign drafts with individual personalization.';
